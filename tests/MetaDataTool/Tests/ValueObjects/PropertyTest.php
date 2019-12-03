52d5b7a47fa1ee821313b652f6b280869ffcc7ff
Danny van der Sluijs <danny.vandersluijs@icloud.com>
2019-12-03 21:10:29 +0100

    Add travis, dependabot and mergiy support

--- tests/MetaDataTool/Tests/ValueObjects/PropertyTest.php
+++ tests/MetaDataTool/Tests/ValueObjects/PropertyTest.php
@@ -2,6 +2,7 @@
 
 namespace MetaDataTool\Tests\ValueObjects;
 
+use MetaDataTool\ValueObjects\HttpMethodMask;
 use MetaDataTool\ValueObjects\Property;
 use PHPUnit\Framework\TestCase;
 
@@ -9,10 +10,11 @@ class PropertyTest extends TestCase
 {
     /**
      * @covers \MetaDataTool\ValueObjects\Property
+     * @covers \MetaDataTool\ValueObjects\HttpMethodMask
      */
     public function testValueObjectHoldsAttributes(): void
     {
-        $property = new Property('name', 'type', 'description', true);
+        $property = new Property('name', 'type', 'description', true, HttpMethodMask::all());
 
         self::assertSame('name', $property->getName());
         self::assertSame('type', $property->getType());
@@ -22,13 +24,14 @@ class PropertyTest extends TestCase
 
     /**
      * @covers \MetaDataTool\ValueObjects\Property
+     * @covers \MetaDataTool\ValueObjects\HttpMethodMask
      */
     public function testPropertyCanBeCorrectlySerialised(): void
     {
-        $property = new Property('name', 'type', 'description', true);
+        $property = new Property('name', 'type', 'description', true, HttpMethodMask::all());
 
         self::assertJsonStringEqualsJsonString(
-            '{"name":"name","type":"type","description":"description","primaryKey":true}',
+            '{"name":"name","type":"type","description":"description","primaryKey":true,"supportedMethods":{"get":true,"post":true,"put":true,"delete":true} }',
             json_encode($property)
         );
     }
